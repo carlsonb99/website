8528321a54421be7e44b2a758f42260396fc3c27
Brian Carlson <carlson.b99@gmail.com>
2017-02-23 17:20:41 -0500

    first commit of website

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,23 @@
+class StaticPagesController < ApplicationController
+
+	# Basic Pages
+	def home
+	end
+
+	def help
+	end
+
+	def about
+	end
+
+	def contact
+	end
+
+	# Extra Pages
+	def media
+	end
+	
+	def tools
+	end
+	
+end
